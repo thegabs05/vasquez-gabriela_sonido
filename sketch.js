84b8470f616846f6614e006c0bb64ad8ea0c73c1
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-10-26 19:54:35 -0500

    inicio del proyecto

---
+++ sketch.js
@@ -0,0 +1,19 @@
+let osc, freq, amp, amp2;
+let t = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  osc = new p5.Oscillator("sawtooth");
+}
+
+function draw() {
+  background(220);
+  freq = constrain(map(noise(t), 0, 1, 500, 900), 500, 900);
+  amp = 0.5;
+
+  //osc.pan(paneo);
+
+  osc.freq(freq, 0.1);
+  osc.amp(amp, 0.1);
+  t += 0.01;
+}
